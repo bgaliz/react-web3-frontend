5ff4004f40095f5845ed07ca176254425ee9231b
Bruno Galiz de Oliveira <brunogaliz@hotmail.com>
2022-07-08 17:15:45 -0400

    Feito acesso com a MetaMask, onde a mesma pede confirmação de acesso

--- src/Hooks/metamask-provider.tsx
+++ src/Hooks/metamask-provider.tsx
@@ -2,7 +2,9 @@ import React, { useState, useContext, useEffect, PropsWithChildren } from 'react
 import { createContext } from 'react';
 import detectEthereumProvider from '@metamask/detect-provider';
 
-interface ContextProps {}
+interface ContextProps {
+  currentProvider: any
+}
 
 interface MetaMaskProviderProps {
     children?: React.ReactNode;
@@ -11,36 +13,91 @@ interface MetaMaskProviderProps {
 const MetaMaskContext = createContext({} as ContextProps);
 
 const MetaMaskProvider: React.FC<PropsWithChildren<MetaMaskProviderProps>> = ( { children } ) => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [currentChainID, setCurrentChainID] = useState('');
+  const [currentProvider, setCurrentProvider] = useState<any>(null);
 
     useEffect(() => {
-        handleDetectEthereumProvider();
-    })
-    
-    async function handleDetectEthereumProvider() {
-        console.log('ue');
-        const provider = await detectEthereumProvider();
-      
-        if (provider) {
-            // initialize your app
-            alert('Starting App!');
-            handleStartApp(provider);
+      if(!currentProvider){
+        handleGetNewCurrentProvider();
+      } else {
+        if(!currentChainID) {
+          handleGetNewChainID();
+        }
+        handleStartApp();
+      }
+    },[currentProvider, currentAccount])
+
+    async function handleGetNewCurrentProvider() {
+        const provider = await detectEthereumProvider({mustBeMetaMask: true});
+
+        if(provider){
+          setCurrentProvider(provider);
         } else {
-            alert('Please install MetaMask!');
+          console.log('Please install MetaMask!');
         }
     }
 
-    function handleStartApp(provider: unknown) {
-        // If the provider returned by detectEthereumProvider is not the same as
-        // window.ethereum, something is overwriting it, perhaps another wallet.
-        if (provider !== window.ethereum) {
-          console.error('Do you have multiple wallets installed?');
-        }
-        // Access the decentralized web!
+    async function handleStartApp() {
+      // If the provider returned by detectEthereumProvider is not the same as
+      // window.ethereum, something is overwriting it, perhaps another wallet.
+      if (currentProvider !== window.ethereum) {
+        console.error('Do you have multiple wallets installed?');
+      }
+      
+      await handleConnectAccount();
+      // Access the decentralized web!
+    }
+
+    async function handleGetNewChainID () {
+      const chainId = await currentProvider.request({ method: 'eth_chainId' });
+      handleChainChanged(chainId);
+      currentProvider.on('chainChanged', handleChainChanged);
+    }
+
+    async function handleChainChanged(_chainId: string) {
+      // We recommend reloading the page, unless you must do otherwise
+        setCurrentChainID(_chainId)
+    }
+
+    async function handleConnectAccount(){
+      try {
+        await currentProvider
+          .request({ method: 'eth_requestAccounts', params: [] })
+          .then(handleAccountsChanged)
+          .catch((err: any) => {
+            // Some unexpected error.
+            // For backwards compatibility reasons, if no accounts are available,
+            // eth_accounts will return an empty array.
+            console.error('batata',err);
+          });
+
+        // Note that this event is emitted on page load.
+        // If the array of accounts is non-empty, you're already
+        // connected.
+        await currentProvider.on('accountsChanged', handleAccountsChanged);
+      }catch (error) {
+        throw Error('Deu ruim')
+      }
+    }
+
+    // For now, 'eth_accounts' will continue to always return an array
+    function handleAccountsChanged(accounts: any) {
+      console.log(accounts)
+      if (accounts.length === 0) {
+        // MetaMask is locked or the user has not connected any accounts
+        console.log('Please connect to MetaMask.');
+      } else if (accounts[0] !== currentAccount) {
+        setCurrentAccount(accounts[0]);
+        // Do any other work!
+      }
     }
 
     return (
         <MetaMaskContext.Provider
-          value={{}}
+          value={{
+            currentProvider
+          }}
         >
           {children}
         </MetaMaskContext.Provider>
--- src/Views/Home/index.tsx
+++ src/Views/Home/index.tsx
@@ -1,5 +1,16 @@
+import './styles.css';
+import { useMetaMaskContext } from '../../Hooks/metamask-provider';
+
 export const Home = () => {
+    const { currentProvider } = useMetaMaskContext();
+
     return(
-        <h1>Hello World!</h1>
+        <div>
+            <h1>MetaMask</h1>
+            {
+                currentProvider && currentProvider.isConnected() ? 'true': 'false'
+            }
+            <button>Entrar</button>
+        </div>
     )
 }
